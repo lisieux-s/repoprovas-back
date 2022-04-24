11f311b11112a59969e859d704c30762379619c5
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-04-23 21:01:43 -0300

    correction to gitignore

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import express, { json } from express;
 import 'express-async-errors';
 
 import './setup.js'
+import router from './routers/index.js';
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(router)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
