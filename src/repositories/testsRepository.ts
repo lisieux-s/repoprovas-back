a12aca2f726fa396c8056ff3867df970efdaa725
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-04-25 16:54:50 -0300

    feat: get disciplines, get teachers

--- src/repositories/testsRepository.ts
+++ src/repositories/testsRepository.ts
@@ -1,30 +1,74 @@
 import { prisma } from '../database.js';
 
 async function findByDiscipline(disciplineId: number) {
-    const resultTeachersDisciplines = await prisma.teacherDiscipline.findMany({
-        where: { disciplineId }
-    })
+  const resultTeachersDisciplines = await prisma.teacherDiscipline.findMany({
+    where: { disciplineId },
+  });
 
-    return resultTeachersDisciplines.map(async (item) => {
-        await prisma.test.findMany({
-            where: { teacherDisciplineId: item.id}
-        })
-    })
+  return resultTeachersDisciplines.map(async (item) => {
+    await prisma.test.findMany({
+      where: { teacherDisciplineId: item.id },
+    });
+  });
 }
 
 async function findByTeacher(teacherId: number) {
-    const resultTeachersDisciplines = await prisma.teacherDiscipline.findMany({
-        where: { teacherId }
-    })
+  const resultTeachersDisciplines = await prisma.teacherDiscipline.findMany({
+    where: { teacherId },
+  });
 
-    return resultTeachersDisciplines.map(async (item) => {
-        await prisma.test.findMany({
-            where: { teacherDisciplineId: item.id}
-        })
-    })
+  return resultTeachersDisciplines.map(async (item) => {
+    await prisma.test.findMany({
+      where: { teacherDisciplineId: item.id },
+    });
+  });
 }
 
+async function findDisciplines() {
+  return await prisma.term.findMany({
+    include: {
+      Discipline: {
+        include: {
+          TeacherDiscipline: {
+            include: {
+              Teacher: true,
+              Test: {
+                include: {
+                  Category: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+async function findTeachers() {
+  return await prisma.teacher.findMany({
+    include: {
+      TeacherDiscipline: {
+        include: {
+          Test: {
+            include: {
+              Category: true,
+            },
+          },
+          Discipline: {
+            include: {
+              Term: true,
+            },
+          },
+        },
+      },
+    },
+  });
+}
 
 export default {
-    findByDiscipline, findByTeacher
-}
+  findByDiscipline,
+  findByTeacher,
+  findDisciplines,
+  findTeachers,
+};
--- src/services/testsService.ts
+++ src/services/testsService.ts
@@ -7,3 +7,11 @@ export async function findByDiscipline(disciplineId: number) {
 export async function findByTeacher(teacherId: number) {
     return await testsRepository.findByTeacher(teacherId);
 }
+
+export async function findDisciplines() {
+    return await testsRepository.findDisciplines();
+}
+
+export async function findTeachers() {
+    return await testsRepository.findTeachers();
+}
